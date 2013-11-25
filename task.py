6313d490953b4654564f567ee290fbd40523a793
Chris Smiles <csmiles000@gmail.com>
2013-11-25 18:45:15 -0500

    first commit

---
+++ task.py
@@ -0,0 +1,17 @@
+# task class
+class Task:
+    
+    # constructor
+    def __init__(self, name, skill_reqs = []):
+        self.name = name
+        self.skill_reqs = skill_reqs
+
+    # other stuff
+    def __str__(self):
+        return self.name
+    
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and self.name == other.name)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
---
+++ worker.py
@@ -0,0 +1,19 @@
+# task class
+class Worker:
+    
+    # constructor
+    def __init__(self, name, skills = [], effs = {}, prefs = {}):
+        self.name = name
+        self.skills = skills
+        self.effs = effs
+        self.prefs = prefs
+
+    # other stuff
+    def __str__(self):
+        return self.name
+    
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and self.name == other.name)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
